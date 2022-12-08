e1aa4c44f1c24ce1d5dfb6cb3e85fdd86fac994d
Kseniya Elsukova <kseniyaels@yandex.ru>
2022-12-08 23:11:08 +0300

    Задача3

---
+++ Задача3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите цифру: ");
+string day = Console.ReadLine() ?? "";
+
+if (day == "1")
+{
+    Console.WriteLine("Понедельник.Рабочий день");
+}
+else if (day == "2")
+{
+    Console.WriteLine("Вторник.Рабочий день");
+}
+else if (day == "3")
+{
+    Console.WriteLine("Среда.Рабочий день");
+}
+else if (day == "4")
+{
+    Console.WriteLine("Четверг.Рабочий день");
+}
+else if (day == "5")
+{
+    Console.WriteLine("Пятница.Рабочий день");
+}
+else if (day == "6")
+{
+    Console.WriteLine("Суббота.Выходной день");
+}
+else if (day == "7")
+{
+    Console.WriteLine("Воскресение.Выходной день");
+}
